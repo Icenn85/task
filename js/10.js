ce04aba794e7d700efbe9443e7feb46f5e6f37ee
Anastasiia Yavtushenko <Icenn85@gmail.com>
2022-10-10 18:57:48 +0300

    add changes task 10js

--- js/10.js
+++ js/10.js
@@ -1,11 +1,11 @@
 const btn = document.getElementById("btn");
 
 function fnBtn() {
+  const btnNext = document.createElement("button");
+  btnNext.innerHTML = "I am a next button";
     return function () {
-        const btnNext = document.createElement("button");
-        btnNext.innerHTML = "I am a next button";
         return document.body.appendChild(btnNext);
   };
 }
 let nextBtn = fnBtn();
-btn.addEventListener("click", nextBtn);
+btn.addEventListener("click", nextBtn); 
